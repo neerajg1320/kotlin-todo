52f8c0182ac57627ed3b8b4a5fbfe98f8d8bf64c
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-16 14:22:32 +0530

    Fixed ListFragment

--- app/src/main/java/com/example/todo_cleanarch/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo_cleanarch/fragments/list/ListFragment.kt
@@ -5,14 +5,18 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_cleanarch.R
 import com.example.todo_cleanarch.databinding.FragmentListBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
-    private var _binding: FragmentListBinding? = null
-    private val binding get() = _binding!!
+//    private val adapter = ListAdapter by lazy { ListAdapter() }
+    private val adapter = ListAdapter()
+
+//    private var _binding: FragmentListBinding? = null
+//    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,9 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = adapter
 
         view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
