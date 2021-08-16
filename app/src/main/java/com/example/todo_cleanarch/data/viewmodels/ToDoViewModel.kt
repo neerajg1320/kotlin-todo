f3b71ab7f28637b7f76a3a5fc8e355b37d0b0b01
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-16 15:06:24 +0530

    Added support for RecyclerView

--- app/src/main/java/com/example/todo_cleanarch/data/viewmodels/ToDoViewModel.kt
+++ app/src/main/java/com/example/todo_cleanarch/data/viewmodels/ToDoViewModel.kt
@@ -13,12 +13,14 @@ import kotlinx.coroutines.launch
 class ToDoViewModel(application: Application): AndroidViewModel(application) {
     private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
     private val repository: ToDoRepository = ToDoRepository(toDoDao)
-    private val getAllData: LiveData<List<ToDoData>> = repository.getAllData
+
+    val getAllData: LiveData<List<ToDoData>> = repository.getAllData
 
     fun insertData(toDoData: ToDoData) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.insertData(toDoData)
         }
     }
+
 
 }
--- app/src/main/java/com/example/todo_cleanarch/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo_cleanarch/fragments/list/ListFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_cleanarch.R
-import com.example.todo_cleanarch.databinding.FragmentListBinding
+import com.example.todo_cleanarch.data.viewmodels.ToDoViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+    private val mToDoViewModel:ToDoViewModel by viewModels()
 
 //    private val adapter = ListAdapter by lazy { ListAdapter() }
     private val adapter = ListAdapter()
@@ -27,6 +31,11 @@ class ListFragment : Fragment() {
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { dataList ->
+            adapter.setData(dataList)
+        })
 
         view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
