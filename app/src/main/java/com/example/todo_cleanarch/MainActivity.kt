392fe5bb1bc0d36c6970dd2c9974a2bc98342362
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-17 17:25:01 +0530

    Replaced fragment with FragmentContainerView in the MainActivity

--- app/src/main/java/com/example/todo_cleanarch/MainActivity.kt
+++ app/src/main/java/com/example/todo_cleanarch/MainActivity.kt
@@ -2,21 +2,28 @@ package com.example.todo_cleanarch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 val TAG:String = "ToDoApp"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.navHostFragment)
+
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
